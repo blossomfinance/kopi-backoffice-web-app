ebd9a78a1062438f59cbe00f522e8c93c8d1640a
Chairul <chairulakmal@live.com>
2021-06-30 14:26:41 +0700

    add authorization headers for search service

--- src/app/search/search.service.ts
+++ src/app/search/search.service.ts
@@ -1,6 +1,8 @@
 /** Angular Imports */
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpBackend, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpBackend, HttpHeaders, HttpParams } from '@angular/common/http';
+import { AuthenticationService } from '../core/authentication/authentication.service';
+
 
 /** rxjs Imports */
 import { Observable } from 'rxjs';
@@ -12,15 +14,19 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class SearchService {
+  authenticationKey: string;
 
   /**
    * @param {HttpClient} http Http Client to send requests.
    */
   constructor(
     private http: HttpClient,
-    private backend: HttpBackend
+    private backend: HttpBackend,
+    private authenticationService: AuthenticationService
   ) {
     this.http = new HttpClient(backend);
+    const savedCredentials = this.authenticationService.getCredentials();
+    this.authenticationKey = savedCredentials.base64EncodedAuthenticationKey;
   }
 
 
@@ -30,12 +36,13 @@ export class SearchService {
    * @returns {Observable<any>} Search Results.
    */
   getSearchResults(search: string, resource: string): Observable<any> {
-    const httpParams = new HttpParams()
+    const headers = new HttpHeaders({ 'Authorization': `Basic ${this.authenticationKey}` });
+    const params = new HttpParams()
       .set('search', search)
       .set('resource', resource)
       .set('exactMatch', 'false');
-    console.debug('Search params', httpParams);
-    return this.http.get('https://koppi-search.herokuapp.com/', { params: httpParams });
+    console.debug('Search headers', headers);
+    return this.http.get('https://koppi-search.herokuapp.com/', { headers, params });
   }
 
 }
